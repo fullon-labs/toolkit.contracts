9554036016799cfdbf86f9c476f7ffbe7b278b5d
Joss <281900360@qq.com>
2025-03-25 15:53:27 +0800

    update

--- contracts/flon.test/include/testcontract.hpp
+++ contracts/flon.test/include/testcontract.hpp
@@ -1,6 +1,20 @@
 #include <eosio/eosio.hpp>
 using namespace eosio;
 
+struct [[eosio::table, eosio::contract("flon.test")]] plan_t {
+   
+   uint64_t id = 0;
+   name    account;
+   std::string  msg;
+   uint64_t primary_key() const { return id; }
+   uint64_t scope() const { return account.value; }
+
+   typedef eosio::multi_index<"plan"_n, plan_t,
+      indexed_by<"by.account"_n, const_mem_fun<plan_t, uint64_t, &plan_t::scope >>
+      >idx_t;
+   EOSLIB_SERIALIZE(plan_t, (id)(account)(msg))
+};
+
 class [[eosio::contract("flon.test")]] testcontract: public eosio::contract {
    public:
       using contract::contract;
@@ -10,6 +24,15 @@ class [[eosio::contract("flon.test")]] testcontract: public eosio::contract {
 
       [[eosio::action]] 
       void check( name nm );
+
+      [[eosio::action]] 
+      void add(name acct, std::vector<std::string> messages);
+
+      [[eosio::action]]
+      void update(name nm, uint64_t id, std::string msg);
+
+      [[eosio::action]]
+      void remove(std::vector<name> accts);
       
       [[eosio::action]]
       std::pair<int, std::string> checkwithrv( name nm );
--- contracts/flon.test/src/testcontract.cpp
+++ contracts/flon.test/src/testcontract.cpp
@@ -25,3 +25,40 @@ std::pair<int, std::string> testcontract::checkwithrv( name nm ) {
    }
    return results; // the `set_action_return_value` intrinsic is invoked automatically here
 }
+
+[[eosio::action]]
+void testcontract::add(name acct, std::vector<std::string> messages) {
+   require_auth(acct);
+   for (auto msg : messages) {
+      plan_t::idx_t plans(_self, acct.value);
+      plans.emplace(acct, [&](auto& item) {
+         item.id = plans.available_primary_key();
+         item.account = acct;
+         item.msg = msg;
+      });
+   }
+}
+
+[[eosio::action]]
+void testcontract::remove(std::vector<name> accts) {
+   for (auto acct : accts) {
+      plan_t::idx_t plans(_self, acct.value);
+      auto itr = plans.begin();
+      while (itr != plans.end()) {
+         itr = plans.erase(itr);
+      }
+   }
+}
+
+[[eosio::action]]
+void testcontract::update(name acct, uint64_t id, std::string msg) {
+   require_auth(acct);
+   
+   plan_t::idx_t plans(_self, acct.value);
+   auto itr = plans.find(id);
+   eosio::check(itr != plans.end(), "Record not found.");
+   plans.modify(itr, acct, [&](auto& item) {
+      item.msg = msg;
+   });
+
+}
