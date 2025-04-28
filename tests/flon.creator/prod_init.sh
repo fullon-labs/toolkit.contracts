6edc6944e0d37fea6efe50439aff1eef36ca43ee
Joss <281900360@qq.com>
2025-04-28 09:59:25 +0800

    update

--- tests/flon.creator/prod_init.sh
+++ tests/flon.creator/prod_init.sh
@@ -6,14 +6,18 @@ source ~/.bashrc
 
 # 设置变量
 con="flon.creator"
-FROM_ACCOUNT="flontest"
+FROM_ACCOUNT="flon"
 TRANSFER_AMOUNT="1000 FLON"
 
 # 检查账号是否存在
 mcli get account "$con"
 
 # 如果账号不存在则创建（你得自己判断上一步是否出错）
-mreg flon "$con" 
+# mreg flon securewarden
+# mreg flon "$con" 
+
+mnew securewarden
+mnew "$con"
 sleep 1
 
 # 部署合约代码
