b77e6f60d62b90eee85d16f9e23c08a3d30d6c5d
Joss <281900360@qq.com>
2025-06-21 20:15:28 +0800

    add flon redpack

--- tests/flon.vest/init.sh
+++ tests/flon.vest/init.sh
@@ -17,8 +17,6 @@ mcli set account permission $con active --add-code
 # 合约初始化: ["admin账户","方案费用 asset", "费用收款账户"]
 mpush $con init '["flonian", "0.00000000 FLON", "flonadmin"]' -p $con@active
 
-
-
 con=flon.vest
 
 recv=flonvestrecv  # 实际领取人，可另设
@@ -39,24 +37,3 @@ mpush flon.token transfer '["'"${issuer}"'", "'"${con}"'", "10.00000000 FLON", "
 
 
 
-
-recv=flonvestrecv  # 实际领取人，可另设
-owner=flonian      # 锁仓创建人
-issuer=flonian     # 发起锁仓转账（和 owner 相同，演示用）
-title="锁仓计划"
-begDays=1
-con=flon.vest  # 你的合约
-plan_id=8
-for times in {1200..1230}; do
-  echo "# 创建锁仓方案, times=$times"
-  mpush "$con" addplan \
-    "[\"$owner\", \"$title$times\", \"flon.token\", \"8,FLON\", $begDays, $times]" -p "$owner"
-  sleep 1
-#   # 获取最新 plan_id（假设顺序创建，直接取最后一条）
-# plan_id=$(mcli get table "$con" "$con" plans 2>/dev/null | awk '/^{/{flag=1}flag' | jq -r '.rows[-1].id')
-# echo "# plan_id=$plan_id"
-    plan_id=${plan_id}+1
-  # 实际锁仓资产给合约，比如都锁10个 FLON
-  mpush flon.token transfer \
-    "[\"$issuer\", \"$con\", \"10.00000000 FLON\", \"issue:$recv:$plan_id:$begDays\"]" -p "$issuer"
-done
