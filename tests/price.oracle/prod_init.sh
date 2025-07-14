0cbda45c28f9cb806718371bcf5abe9879c15791
Joss <281900360@qq.com>
2025-07-14 14:24:20 +0800

    add oracle shell

--- tests/price.oracle/prod_init.sh
+++ tests/price.oracle/prod_init.sh
@@ -12,3 +12,11 @@ mreg flon $bid flonian
 mset $con price.oracle
 
 mpush $con addoracle '[ "'$bid'" ]' -p $con
+mpush $con addcoin '[ "btc" ]' -p $con
+mpush $con addcoin '[ "eth" ]' -p $con
+mpush $con addcoin '[ "bnb" ]' -p $con
+
+
+mpush $con updateprice '[ "'$bid'", [{"tpcode":"btc.usdt","price":"117951.220000 USDT"},{"tpcode":"eth.usdt","price":"2968.110000 USDT"}] ]' -p $bid
+
+
